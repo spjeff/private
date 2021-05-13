a667424c68dcc3225ad60c6a0f2c14c494805224
Jeff Jones <spjeff@spjeff.com>
2021-05-13 08:23:46 -0500

    .

--- _timesheet/timesheet-notes-fix.ps1
+++ _timesheet/timesheet-notes-fix.ps1
@@ -1,35 +1,51 @@
+# Input full CSV 
 # Format daily work notes into 5 even blocks for timesheet
 
-# Read source
-$file = "timesheet"
-$txt = Get-Content "$file.txt"
 
-# Count max length and clean whitespace
-$length = 0
-$coll = @()
-foreach ($line in $txt) {
-    $line = $line.Trim()
-    if (!$line.EndsWith(".")) {
-        $line += "."
+function ProcessClient ($client, $clientRows) {
+    # Read source
+    # $file = "timesheet"
+    # $txt = Get-Content "$file.txt"
+
+    
+
+    # Count max length and clean whitespace
+    $length = 0
+    $coll = @()
+    foreach ($line in $txt) {
+        $line = $line.Trim()
+        if (!$line.EndsWith(".")) {
+            $line += "."
+        }
+        $coll += $line
+        $length += $line.Length
     }
-    $coll += $line
-    $length += $line.Length
+
+    # Break into daily blocks
+    $j=0
+    $daylength = $length/5
+    $daynotes = @('* ','* ','* ','* ','* ')
+    for ($i=0; $i -le 4; $i++) {
+        $note = ''
+        $notelength = 0
+        while ($notelength -lt $daylength) {
+            $note += $coll[$j] + " "
+            $j++
+            $notelength += $note.length
+        }
+        $daynotes[$i] += $note
+    }
+
+    # Write final TXT
+    $daynotes | Out-File "$client.txt" -Force
 }
 
-# Break into daily blocks
-$j=0
-$daylength = $length/5
-$daynotes = @('* ','* ','* ','* ','* ')
-for ($i=0; $i -le 4; $i++) {
-    $note = ''
-    $notelength = 0
-    while ($notelength -lt $daylength) {
-        $note += $coll[$j] + " "
-        $j++
-        $notelength += $note.length
+function Main() {
+    $csv = Import-Csv "timesheet.csv"
+    $groups = $csv | Group Client
+    foreach ($client in $groups) {
+        $clientRows = ($csv |? {$_.Client -eq $client})
+        ProcessClient $client $clientRows
     }
-    $daynotes[$i] += $note
 }
-
-# Write final TXT
-$daynotes | Out-File "fix.txt" -Force
+Main
